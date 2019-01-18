97148ee692114c747d2c7923eca57ea65b8b3423
Thomas Tunc <thomastunc@hotmail.com>
2019-01-18 10:45:03 +0100

    Added the basics

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        new Main().run();
+    }
 
+    private void run() {
+        Maze maze = new Maze();
+        maze.start();
     }
 }
